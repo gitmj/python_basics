4785a409d9dde405ad15e47dc1fa0f647afed0d5
mj <manoj41@gmail.com>
2016-02-20 17:06:12 -0800

    cleanup  Changes to be committed: 	new file:   google-python-exercises/LICENSE.txt 	new file:   google-python-exercises/NOTICE.txt 	new file:   google-python-exercises/babynames/baby1990.html 	new file:   google-python-exercises/babynames/baby1992.html 	new file:   google-python-exercises/babynames/baby1994.html 	new file:   google-python-exercises/babynames/baby1996.html 	new file:   google-python-exercises/babynames/baby1998.html 	new file:   google-python-exercises/babynames/baby2000.html 	new file:   google-python-exercises/babynames/baby2002.html 	new file:   google-python-exercises/babynames/baby2004.html 	new file:   google-python-exercises/babynames/baby2006.html 	new file:   google-python-exercises/babynames/baby2008.html 	new file:   google-python-exercises/babynames/babynames.py 	new file:   google-python-exercises/babynames/solution/babynames.py 	new file:   google-python-exercises/basic/alice.txt 	new file:   google-python-exercises/basic/list1.py 	new file:   google-python-exercises/basic/list2.py 	new file:   google-python-exercises/basic/mimic.py 	new file:   google-python-exercises/basic/small.txt 	new file:   google-python-exercises/basic/solution/list1.py 	new file:   google-python-exercises/basic/solution/list2.py 	new file:   google-python-exercises/basic/solution/mimic.py 	new file:   google-python-exercises/basic/solution/string1.py 	new file:   google-python-exercises/basic/solution/string2.py 	new file:   google-python-exercises/basic/solution/wordcount.py 	new file:   google-python-exercises/basic/string1.py 	new file:   google-python-exercises/basic/string2.py 	new file:   google-python-exercises/basic/wordcount.py 	new file:   google-python-exercises/copyspecial/copyspecial.py 	new file:   google-python-exercises/copyspecial/solution/copyspecial.py 	new file:   google-python-exercises/copyspecial/xyz__hello__.txt 	new file:   google-python-exercises/copyspecial/zz__something__.jpg 	new file:   google-python-exercises/hello.py 	new file:   google-python-exercises/logpuzzle/animal_code.google.com 	new file:   google-python-exercises/logpuzzle/logpuzzle.py 	new file:   google-python-exercises/logpuzzle/place_code.google.com 	new file:   google-python-exercises/logpuzzle/solution/logpuzzle.py 	new file:   hw1/hw1.ipynb 	new file:   list_comprehension.py 	new file:   mergesort.py 	new file:   module.py

---
+++ list_comprehension.py
@@ -0,0 +1,6 @@
+ppl = ['Alice', 'Bob', 'Carol', 'Doug']
+excited_ppl = [e + '-!!' for e in ppl]
+print excited_ppl
+ppl_len = [len(x) for x in ppl]
+print ppl_len
+
---
+++ module.py
@@ -0,0 +1,2 @@
+import fib
+fib.fib(5)
